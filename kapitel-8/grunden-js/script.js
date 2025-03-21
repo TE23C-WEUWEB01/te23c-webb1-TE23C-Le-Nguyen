b7ac2b1dff7407577fd550c52375f353cdff5a33
Le gia Nguyen <le.gia@elev.ga.ntig.se>
2025-03-21 09:44:12 +0100

    tränant på if-satser och loopar

---
+++ kapitel-8/grunden-js/script.js
@@ -0,0 +1,23 @@
+// while (true) {
+//     var namn = prompt("ange ditt namn:");
+//     var namn = prompt("ange ditt lösenord:");
+//     if (lösenord == "12345") {
+//         document.writeln(`Du ${namn} är inloggad`);
+//         break;
+//     }
+//     else {
+//         document.writeln(`du ${namn} har skrivit fel lösenord`);
+//     }
+// }
+
+// for (let index = 20; index < 31; index++) {
+//    document.writeln(`tal: ${index} <br>`);
+    
+// }
+
+//let val = confirm("klicka OK för att fortsätta") //true eller false
+let val = true
+while (val)
+{
+    let val = confirm("klicka OK för att fortsätta") //true eller false
+}
