6ace1a30e3fd5a6c769908d88d7ee901a97bb8cd
Le gia Nguyen <le.gia@elev.ga.ntig.se>
2025-04-10 14:30:39 +0200

    tränant på click-event , ändra stil, och dölja/visa text o bild

---
+++ kapitel-8/click event/script.js
@@ -0,0 +1,20 @@
+    function setDarkMode() {
+        document.body.style = "background: #000; color: #fff;";
+    }
+    function setLightMode() {
+        document.body.style = "background: #fff; color: #000;";
+    }
+    function hideMode() {
+        document.querySelector("h1").style = "visibility: hidden";
+    }
+    function showMode() {
+        document.querySelector("h1").style = "visibility: on";
+    }
+    function hidewordMode() {
+        document.querySelector("P").style = "visibility: hidden";
+    }
+    function wordMode() {
+        document.querySelector("P").style = "visibility: visible";
+    }
+
+    
